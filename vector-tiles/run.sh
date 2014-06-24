97238c1418e89b34a7fbd528c96d571730b79bee
fpulido <fpulido@becario.(none)>
2014-06-24 09:31:01 +0200

    update runnable jar name

--- vector-tiles/run.sh
+++ vector-tiles/run.sh
@@ -3,5 +3,5 @@
 ## igosoftware.dyndns.org 5414 postgres postgres1g0 vectorial_test false 0 3 ne_10m_admin_0_countries true continent mapcolor7 scalerank
 
 
-java -jar vectorialLOD.jar $1
+java -jar mmtVectorTiles.jar $1
 
